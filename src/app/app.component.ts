8e28ca92e6cac590d5ede453a3a02004ab2f9d8b
ImsirovicAjdin <ajdika@live.com>
2019-10-09 13:01:49 +0200

    Definitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,3 +29,12 @@ Task to complete:
 Define an observable that emits three strings and subscribe with method for next(), error(), and complete()
 
 */
+
+/*
+Definitions:
+Observable: Any stream or conveyor of data;
+Observer: A set of methods to observe and respond to the stream
+Subscriber: An Observer that can unsubscribe from an Observable.
+Subscription: Execution of an Observable (i.e === "Move the conveyor!")
+
+*/
