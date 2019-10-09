66ea894338bed21bce4ebf87f5d5af97e11522c8
ImsirovicAjdin <ajdika@live.com>
2019-10-09 12:55:25 +0200

    Use of() Observable creation function to print 4 numbers to console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent  {}
+export class AppComponent  {
+  ngOnInit() {
+    of(2,4,6,8).subscribe(console.log);
+  }
+}
