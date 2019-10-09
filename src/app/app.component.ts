3d5b5e6f281482edff7f6b1ef3f36bc911f9f2c0
ImsirovicAjdin <ajdika@live.com>
2019-10-09 13:06:46 +0200

    How to Create, Start, and Stop an Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,3 +38,33 @@ Subscriber: An Observer that can unsubscribe from an Observable.
 Subscription: Execution of an Observable (i.e === "Move the conveyor!")
 
 */
+
+
+/*
+Create an Observable: 
+1. Constructor
+2. Creation functions: of, from, fromEvent, interval (create an Observable FROM ANYTHING)
+3. Returned from an Angular feature:
+  - Forms: valueChanges
+  - Routing: paramMap
+  - HTTP: get
+
+Start an Observable:
+1. Call subscribe! Pass in an Observer with next(), error(), complete():
+
+  const sub = appleStream.subscribe(
+    apple => console.log(`Emitted: ${apple}`),
+    err => console.log(`Error: ${err}`),
+    () => console.log(`No more apples, go home`)
+  )
+
+Stopping an Observable:
+1. Call complete() on the Observer
+2. Use a creation function THAT COMPLETES: of, from, ...
+3. Use an operator THAT COMPLETES: take,...
+4. Throw an Error
+5. Call unsubscribe() on the Subscription: sub.unsubscribe();
+
+
+
+*/
